4150123cf655023115ce62fe390c1380028816a2
YuryShageev <schageev.yur@yandex.ru>
2022-10-10 12:52:30 +0300

    Начало про массивы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] weights = {90, 92, 95, 98, 88, 96, 97, 93, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+        }
     }
 }
